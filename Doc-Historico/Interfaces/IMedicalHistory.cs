35a80916ec5c88afd2472008162f70d13aa6d56c
adriano.binhara <abinhara@its-training.com>
2023-10-18 09:04:45 -0300

    Endpoint historico medico

--- Doc-Historico/Interfaces/IMedicalHistory.cs
+++ Doc-Historico/Interfaces/IMedicalHistory.cs
@@ -5,7 +5,7 @@ namespace Doc_Historico.Interfaces
 {
 	public interface IMedicalHistory
 	{
-		Task<List<Historico>> GetPatientMedicalHistory(string idUsuario);
+		Task<List<Historico>> GetAllPatientMedicalHistory(string idUsuario);
 		Task<Historico> GetSingleHistoricoPatient(string idHistorico, string idUsuario);
 		Task DeleteHistorico(string idHistorico, string idUsuario);
 		Task<Historico> AddHistoricoPatient(string idUsuario, Historico historico);
